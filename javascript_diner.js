207c71b1a093841eaea4ea185b55a03c65a825db
Chandler Ellis <chandler.ellis47@gmail.com>
2018-05-04 13:03:46 -0600

    Added code to this file, started the project and got the first functionalities working

---
+++ javascript_diner.js
@@ -0,0 +1,71 @@
+// const mainMenu = ['Cheeseburger', 'Steak', 'Shrimp Scampi']{
+//     var user = input('Which Entree would you like? We have a Cheeseburger, Steak, and Shrimp Scampi.')
+//   }
+  
+//   var total = 0
+  
+  
+//   if (user === "Cheeseburger") {
+//     print("Excellent choice! Cheeseburgers go great with fries!")
+//     total = total + 2.25;
+//     print(f'Your price is {total}')
+//     }
+//   else if (user === "Steak") {
+//     print("Steak is my favorite too!")
+//     total = total + 4.45;
+//     print(f'Your price is {total}')
+//   }
+//   else if (user === "Shrimp Scampi") {
+//     print("Really? I'm not a big seafood person, but I hear it's great!")
+//     total = total + 3.55;
+//     print(f'Your price is {total}')
+//   }
+//   else {
+//     print("Sorry, we don't have that.")
+//   }
+  
+  
+//   const sideMenu = ['Salad', 'Soup', 'Fries']{
+//   var user = input('Which side would you like? We have Salad, Soup, and Fries')
+//   }
+  
+//   if (user === "Salad") {
+//     print("What are you a vegan? Just kidding it's really good.")
+//     total = total + 1.2
+//     print(f'Your total price is {total}')
+//     }
+//   else if (user === "Soup") {
+//     print"Mmm, I love soup!")
+//     total = total + 2.25
+//     print(f'Your total price is {total}')
+//   }
+//   else if (user === "Fries") {
+//     print("Great Choice, our fries are World Famous!")
+//     total = total + 2.75
+//     print(f'Your total price is {total}')
+//   }
+//   else {
+//     print("Sorry, we don't have that.")
+//   }
+  
+//   var user = prompt('Which other side would you like? We have Salad, Soup, and Fries')
+  
+//   if (user === "Salad") {
+//     print("What are you a vegan? Just kidding it's really good.") 
+//     total = total + 1.25
+//     print(f'Your total price is {total}')
+//     }
+//   else if (user === "Soup") {
+//     print("Mmm, I love soup!")
+//     total = total + 2.25
+//     print(f'Your current price is $${total}')
+//   }
+//   else if (user === "Fries") {
+//     print("Great Choice, our fries are World Famous!")
+//     total = total + 2.75
+//     print(f'Your total price is {total}')
+//   }
+//   else {
+//     print("Sorry, we don't have that.")
+//   }
+  
--- python_project1.py
+++ python_project1.py
@@ -1,1 +1,46 @@
-print('hello world')
+main_menu = ['King Crab', 'Ribs', 'Beef Wellington'] 
+
+user = input('Please choose one Entree, we are offering King Crab, Ribs, and Beef Wellington tonight ')
+
+
+total = 0
+
+
+if user == "King Crab":
+    print("MMM I love seafood, your total for the crab will be $5.25")
+    total = total + 5.25
+
+elif user == "Ribs":
+    print("Our Ribs are the finest in town! Your total for the ribs will be $4.50")
+    total = total + 4.50
+elif user == "Beef Wellington":
+    print("Chef Ramsey's favorite, that will be $3.75")
+    total = total + 3.75
+else:
+    print("My apologies, I don't believe we carry that")
+
+
+side_menu = ['Potatoes', 'Steak Fries', 'Cheese Curds']
+
+user = input('Please choose a side with your meal! We have Potatoes, Steak Fries, and Cheese Curds. ')
+
+if user == "Potatoes":
+    print("Awesome, we take pride in our 'taters! ")
+    total = total + 1.25
+    print(f'Your current total is ${total}')
+
+elif user == "Steak Fries":
+    print("Yum! Nothing beats fries!")
+    total = total + 1.75
+    print(f'Your current total is ${total}')
+
+elif user == "Cheese Curds":
+    print("Gotta love them cheese curds, I tell ya what")
+    total = total + 2.50
+    print(f'Your current total is ${total}')
+
+else:
+    print('Sorry we don\'t have that tonight!')
+
+
+
